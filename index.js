2fe7a60ac5a4f31d864e41703f1fd41f59e9ac42
John DH. Park <john.dh.park@me.com>
2023-04-17 07:55:23 -0700

    completed sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+	const body = document.querySelector("body");
+
+	return fetch("http://localhost:3000/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify({
+			name,
+			email,
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			const p = document.createElement("p");
+			p.textContent = data.id;
+
+			body.appendChild(p);
+		})
+		.catch((err) => {
+			const p = document.createElement("p");
+			p.textContent = err.message;
+
+			body.appendChild(p);
+		});
+}
